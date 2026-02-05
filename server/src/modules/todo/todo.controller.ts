c419985abb869edc139f1f6d75c999611a6c121b
Idris Nurzhanov <idrisnurzhanov@MacBook-Air-Idris.local>
2026-02-05 19:35:03 +0600

    fixed misstakes in controllers

--- server/src/modules/todo/todo.controller.ts
+++ server/src/modules/todo/todo.controller.ts
@@ -2,28 +2,40 @@ import { Request, Response } from "express";
 import { prisma } from "../../config/prisma";
 
 const getTodo = async (Request: Request, Response: Response) => {
-  const todos = await prisma.todo.findMany();
-  Response.json(todos);
+  try {
+    const todos = await prisma.todo.findMany();
+    Response.status(200).json(todos);
+  } catch (error) {
+    Response.status(500).json({ error: "Failed to fetch todos" });
+  }
 };
 
 const postTodo = async (Request: Request, Response: Response) => {
-  const { title } = Request.body;
-  const todo = await prisma.todo.create({
-    data: {
-      title,
-    },
-  });
-  Response.json(todo);
+  try {
+    const { title } = Request.body;
+    const todo = await prisma.todo.create({
+      data: {
+        title,
+      },
+    });
+    Response.status(201).json(todo);
+  } catch (error) {
+    Response.status(500).json({ error: "Failed to create todo" });
+  }
 };
 
 const deleteTodo = async (Request: Request, Response: Response) => {
-  const { id } = Request.params;
-  const todo = await prisma.todo.delete({
-    where: {
-      id: Number(id),
-    },
-  });
-  Response.json(todo);
+  try {
+    const { id } = Request.params;
+    const todo = await prisma.todo.delete({
+      where: {
+        id: Number(id),
+      },
+    });
+    Response.status(200).json(todo);
+  } catch (error) {
+    Response.status(500).json({ error: "Failed to delete todo" });
+  }
 };
 
 export default {
