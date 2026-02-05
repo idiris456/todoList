dca0777e9649fc83212b13b95ffede8e5d16bc03
Zhoodarbeshim <joodarbeshimaktanov@gmail.com>
2026-02-05 18:16:26 +0600

    server ready

---
+++ server/src/index.ts
@@ -0,0 +1,24 @@
+const server = buildServer()
+const PORT = Number(process.env.PORT);
+const startServer = async () => {
+  try {
+    new Promise<void>((resolve, reject) => {
+      server.lister(
+        {
+          port: PORT,
+          host: "0.0.0.0",
+        },
+        () => {
+          console.log(`${new Date()}`);
+          console.log(`Server run in: http://localhost:${PORT}`);
+        },
+      );
+    });
+  } catch (error) {
+    console.log(
+        console.log(`Server crashed: ${error}`);
+        process.exit(1);
+    );
+  }
+};
+startServer();
